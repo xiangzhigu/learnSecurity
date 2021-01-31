56e123a678e405ac7d3275a3cbfea1cce181e3bd
xiangzhigu <gxz16120341222@163.com>
2021-01-31 15:48:22 +0800

    first

---
+++ learnSecurity/src/test/java/com/xiangzhi/demo/LearnSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xiangzhi.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
